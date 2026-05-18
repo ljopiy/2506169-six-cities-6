27d1c77c26c42be7ae2f71831d8efe543eca45ae
ljopy <ahusainova96@gmail.com>
2026-05-18 16:51:42 +0500

    fix: eslint 5.0

--- src/shared/modules/comment/dto/create-comment.dto.ts
+++ src/shared/modules/comment/dto/create-comment.dto.ts
@@ -5,7 +5,7 @@ export class CreateCommentDto {
   @IsString({ message: CreateCommentMessages.text.invalidFormat })
   @Length(5, 1024, { message: CreateCommentMessages.text.lengthField })
   public text!: string;
-  
+
   @IsNumber({}, { message: CreateCommentMessages.rating.invalidFormat })
   @Min(1, { message: CreateCommentMessages.rating.rangeField })
   @Max(5, { message: CreateCommentMessages.rating.rangeField })
